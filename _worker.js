8dd25abad3be75075c6186ac1271256c7e541b9b
hadi0702 <120447768+hadi0702@users.noreply.github.com>
2023-08-12 14:46:56 +0330

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ import { connect } from 'cloudflare:sockets';
 // t.me/P_tech2024 
 // How to generate your own UUID:
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
-let userID = '2cd61524-9843-43ab-8a0a-df4a744bb859';
+let userID = '4007bf96-5149-4430-a5c2-1720d63449c0';
 
 const proxyIPs = ['mtn.ircf.space', 'mkh.ircf.space', 'mci.ircf.space', 'rtl.ircf.space'];
 let proxyIP = proxyIPs[Math.floor(Math.random() * proxyIPs.length)];
